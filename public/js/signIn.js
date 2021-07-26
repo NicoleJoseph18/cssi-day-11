0f17317d97cc45b9f43509c21a9829fdabcf7224
NicoleJoseph18 <nicolejosephk64581@gmail.com>
2021-07-26 17:10:01 +0000

    add error handling and connect user

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,5 +1,14 @@
 const signInButton = document.querySelector(".button")
 console.log(signInButton)
+
 signInButton.addEventListener("click", () => {
-console.log("clicked")
+    console.log("clicked")
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user
+        console.log("log in succes!", user)
+    })
+    .catch(error => {
+        console.log("log in failed", error)
+    })
 })
