86c80f245b59f713571ac662d1f0678111b843d6
NicoleJoseph18 <nicolejosephk64581@gmail.com>
2021-07-26 18:58:13 +0000

    not finished attempt at making time stamp

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -1,8 +1,10 @@
 let googleUser = null
+let timeObj = new Date();
 window.onload = () => {   //when window finishes loading
     firebase.auth().onAuthStateChanged(user => {
         if (user) { //if there's a user already logged in
             console.log("logged in as", user.displayName)
+            alert("WELCOME " + user.displayName + "!!!");
             googleUser = user;
         } else {
             console.log("not logged in")
@@ -14,12 +16,14 @@ window.onload = () => {   //when window finishes loading
         //get values from input
         noteTitle = document.querySelector("#noteTitle").value
         noteText = document.querySelector("#noteText").value
+        note
 
 
         //write this data into database
         firebase.database().ref(`/users/${googleUser.uid}`).push({
             title: noteTitle,
             text: noteText
+            time : timeObj.getHours() + ":" + timeObj.getMinutes() + ":" + timeObj.getSeconds();
         }).then(() => {
             console.log("database write successful")
             document.querySelector("#noteTitle").value = ""
